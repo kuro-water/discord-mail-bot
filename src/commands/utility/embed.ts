57a8406bd335b9aada689da0e116e9600bd7c73d
kuro-water <kuro07001@gmail.com>
2025-03-21 21:47:16 +0900

    いい感じの埋め込み作ってみた

--- src/commands/utility/embed.ts
+++ src/commands/utility/embed.ts
@@ -1,13 +1,49 @@
 import {SlashCommand} from "../../@types/types";
-import {ChatInputCommandInteraction, SlashCommandBuilder} from "discord.js";
+import {AttachmentBuilder, ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder} from "discord.js";
+import * as path from "node:path";
 
 export const embed: SlashCommand = {
     data: new SlashCommandBuilder()
         .setName("embed")
-        .setDescription("pingができるよ"),
+        .setDescription("埋め込みメッセージのテストだよ"),
     async execute(interaction: ChatInputCommandInteraction): Promise<void> {
-        const sent = await interaction.reply({content: "Pinging..."});
-        await interaction.followUp(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
-        await interaction.editReply(`Websocket heartbeat: ${interaction.client.ws.ping}ms.`);
+        const filePath = path.join(__dirname, "../../images/icon.jpg");
+        const icon = new AttachmentBuilder(filePath);
+
+        const embed = new EmbedBuilder()
+            // .setColor(0x0099FF)
+            .setURL("https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox")
+            // .setAuthor({
+            //     name: "notify email made by kuro-water",
+            //     iconURL: "attachment://icon.jpg",
+            //     url: "https://github.com/kuro-water/discord-mail-bot"
+            // })
+            .setTitle("To:example.com")
+            // .setDescription("from:example.com")
+            .setAuthor({name: "from:example.com"})
+            // .setThumbnail("https://i.imgur.com/AfFp7pu.png")
+            .addFields(
+                {
+                    name: "[Test] This is Subject",
+                    value: "This is main text\n" +
+                        "hello, discord\n" +
+                        "hello, discord\n" +
+                        "hello, discord\n" +
+                        "hello, discord\n" +
+                        "hello, discord\n"
+                },
+                // {name: "\u200B", value: "\u200B"},
+                // {name: "Inline field title", value: "Some value here", inline: true},
+                // {name: "Inline field title", value: "Some value here", inline: true},
+            )
+            // .addFields({name: "Inline field title", value: "Some value here", inline: true})
+            // .setImage("https://i.imgur.com/AfFp7pu.png")
+            .setTimestamp()
+            .setFooter({
+                text: "notify email made by kuro-water",
+                iconURL: "attachment://icon.jpg",
+            });
+
+        await interaction.reply({embeds: [embed], files: [icon]});
     },
 };
